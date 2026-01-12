de49502a8fe5f5b50d8d6dc0ca5495cf2521074d
piyush_alpha01 <pm455567@gmail.com>
2026-01-12 13:03:23 +0530

    utlils fn added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config();
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`Connected to localhost:${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(err.message);
+});
